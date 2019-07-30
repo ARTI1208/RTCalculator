858d1bf4c6584bfce2bc0143e8afa86df83776f7
ARTI1208 <31630000+ARTI1208@users.noreply.github.com>
2019-07-30 18:46:11 +0300

    [FIX] Android 9+ currency load

--- app/src/main/java/ru/art2000/calculator/currency_converter/CurrencyConverterFragment.java
+++ app/src/main/java/ru/art2000/calculator/currency_converter/CurrencyConverterFragment.java
@@ -8,14 +8,6 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.core.content.ContextCompat;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -24,10 +16,20 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -36,27 +38,41 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import ru.art2000.calculator.R;
 import ru.art2000.extensions.CurrencyItem;
 import ru.art2000.extensions.CurrencyValues;
 
 public class CurrencyConverterFragment extends Fragment {
 
-    private TextView date;
     public Context mContext;
+    public CurrencyListAdapter adapter = null;
+    private TextView date;
     private RecyclerView recycler;
     private Activity parent;
-    public CurrencyListAdapter adapter = null;
     private View v = null;
     private String updDate;
     private SwipeRefreshLayout refresher;
-    private boolean onUpdate;
+    private boolean isUpdating;
 
     public CurrencyConverterFragment() {
         super();
     }
 
-    public void scrollToTop(){
+    private static boolean isOnline() {
+        try {
+            return new checkOnline().execute().get();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private void setRefreshStatus(boolean status){
+        refresher.setRefreshing(status);
+        isUpdating = status;
+    }
+
+    public void scrollToTop() {
         if (recycler != null)
             recycler.smoothScrollToPosition(0);
     }
@@ -90,13 +106,13 @@ public class CurrencyConverterFragment extends Fragment {
             TypedValue accentValue = new TypedValue();
             mContext.getTheme().resolveAttribute(R.attr.colorAccent, accentValue, true);
             refresher.setColorSchemeColors(ContextCompat.getColor(mContext, accentValue.resourceId));
-            refresher.setRefreshing(true);
+            setRefreshStatus(true);
             updateData();
             refresher.setOnRefreshListener(this::updateData);
             date = v.findViewById(R.id.tv_update_date);
             updateDate(null);
             EditText focus = v.findViewById(R.id.focuser);
-            v.findViewById(R.id.edit_currencies).setOnClickListener(v-> {
+            v.findViewById(R.id.edit_currencies).setOnClickListener(v -> {
                 adapter.removeEditText();
                 focus.requestFocus();
                 Intent intent = new Intent(getActivity(), EditShownCurrencies.class);
@@ -106,8 +122,8 @@ public class CurrencyConverterFragment extends Fragment {
         return v;
     }
 
-    private void updateDate(String newDate){
-        parent.runOnUiThread(()->{
+    private void updateDate(String newDate) {
+        parent.runOnUiThread(() -> {
             if (newDate == null) {
                 updDate = CurrencyValues.updateDate;
                 date.setText(updDate);
@@ -118,15 +134,15 @@ public class CurrencyConverterFragment extends Fragment {
         });
     }
 
-    private String getShownCodesSearch(){
+    private String getShownCodesSearch() {
         StringBuilder builder = new StringBuilder();
         String def = "tr:contains(";
         int i = 0;
-        for (CurrencyItem item : CurrencyValues.visibleList){
+        for (CurrencyItem item : CurrencyValues.visibleList) {
             if (i != 0)
                 builder.append(", ");
             builder.append(def).append(item.code).append(")");
-            i=1;
+            i = 1;
         }
         return builder.toString();
     }
@@ -139,45 +155,73 @@ public class CurrencyConverterFragment extends Fragment {
     }
 
 
+    private Double doubleFromString(String string){
+        if (string.contains(",")){
+            string = string.replace(',', '.');
+        }
+        try {
+            return Double.parseDouble(string);
+        } catch (Exception e){
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
     private void parseHTML() {
         new Thread(() -> {
             try {
                 Document webpage = Jsoup.connect("http://www.cbr.ru/currency_base/daily/").get();
-                onUpdate = true;
                 String dateFormat = "[0-9]{2}.[0-9]{2}.[0-9]{4}";
                 Pattern datePattern = Pattern.compile(dateFormat);
-                Matcher matcher = datePattern.matcher(webpage.select("h2").first().text());
-
+                String dateBlock = webpage.select("h2").first().text();
+                Matcher matcher = datePattern.matcher(dateBlock);
                 String date = "";
 
                 if (matcher.find())
-                    date = webpage.select("h2").first().text().substring(matcher.start(), matcher.end());
+                    date = dateBlock.substring(matcher.start(), matcher.end());
 
                 if (updDate.equals(date)) {
-                    refresher.setRefreshing(false);
+                    setRefreshStatus(false);
                     return;
                 }
-                parent.runOnUiThread(()->
+                parent.runOnUiThread(() ->
                         Toast.makeText(mContext, "Обновление...", Toast.LENGTH_SHORT).show());
                 CurrencyDB currencyDB = new CurrencyDB(mContext);
                 ContentValues contentValues = new ContentValues();
                 SQLiteDatabase database = currencyDB.getWritableDatabase();
-                Elements tables = webpage.select("table").first().children().first().children();
-                Double ru_val = Double.parseDouble(tables.select("tr:contains(USD)").first().child(4).text().replace(',', '.'));
+
+                Elements table = null;
+                for (Element el :
+                        webpage.select("table")) {
+                    if (el.hasClass("data")) {
+                        table = el.children().first().children();
+                    }
+                }
+
+                if (table == null) {
+                    parent.runOnUiThread(() ->
+                            Toast.makeText(mContext, "Load failed: update app or contact dev if it is not fixed in last version", Toast.LENGTH_SHORT).show());
+                    return;
+                }
+
+                Double ru_val = Double.parseDouble(table.select("tr:contains(USD)").first().child(4).text().replace(',', '.'));
                 CurrencyValues.updateRate("RUB", ru_val);
                 NumberFormat dot2dig = new DecimalFormat("#.##");
-                tables.remove(0);
-                for (Element child : tables.select(getShownCodesSearch())){
-                    Double values = Double.parseDouble(child.child(4).text().replace(',', '.'));
-                    Double units = Double.parseDouble(child.child(2).text().replace(',', '.'));
-                    Double valuesPerUnit = Double.parseDouble(dot2dig.format(ru_val / (values / units)).replace(',', '.'));
+                table.remove(0);
+                for (Element child : table.select(getShownCodesSearch())) {
+//                    Double value = Double.parseDouble(child.child(4).text().replace(',', '.'));
+//                    Double units = Double.parseDouble(child.child(2).text().replace(',', '.'));
+//                    Double valuePerUnit = Double.parseDouble(dot2dig.format(ru_val * units / value).replace(',', '.'));
+                    Double value = doubleFromString(child.child(4).text());
+                    Double units = doubleFromString(child.child(2).text());
+                    Double valuePerUnit = doubleFromString(dot2dig.format(ru_val * units / value));
                     String letterCode = child.child(1).text();
-                    CurrencyValues.updateRate(letterCode, valuesPerUnit);
+                    CurrencyValues.updateRate(letterCode, valuePerUnit);
                 }
                 updateList();
                 updateDate(date);
 
-                for (Element child : tables) {
+                for (Element child : table) {
 //                                Log.d("Цифр. код", child.child(0).text());
 //                                Log.d("Букв. код", child.child(1).text());
 //                                Log.d("Единиц", child.child(2).text());
@@ -190,46 +234,44 @@ public class CurrencyConverterFragment extends Fragment {
                     CurrencyValues.updateRate(letterCode, valuesPerUnit);
                     contentValues.put("rate", valuesPerUnit);
                     database.update("currency", contentValues,
-                                            "codeLetter = ?", new String[]{letterCode});
+                            "codeLetter = ?", new String[]{letterCode});
                     contentValues.clear();
                 }
                 contentValues.put("rate", ru_val);
                 database.update("currency", contentValues,
                         "codeLetter = ?", new String[]{"RUB"});
                 currencyDB.close();
-                refresher.setRefreshing(false);
+                setRefreshStatus(false);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }).start();
     }
 
-    private void updateList(){
+    private void updateList() {
         if (adapter != null) {
-            parent.runOnUiThread(()->
-                adapter.getDataFromDB());
+            parent.runOnUiThread(() ->
+                    adapter.getDataFromDB());
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setRefreshStatus(isUpdating);
     }
 
     private void updateData() {
         if (isOnline()) {
             parseHTML();
         } else {
-            refresher.setRefreshing(false);
+            setRefreshStatus(false);
             Toast.makeText(mContext, "Failed: no Internet", Toast.LENGTH_SHORT).show();
             updateList();
         }
     }
 
-    private static boolean isOnline() {
-        try {
-            return new checkOnline().execute().get();
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    static class checkOnline extends AsyncTask<Void, Void, Boolean>{
+    static class checkOnline extends AsyncTask<Void, Void, Boolean> {
 
         @Override
         protected Boolean doInBackground(Void... voids) {
@@ -240,7 +282,7 @@ public class CurrencyConverterFragment extends Fragment {
                 sock.connect(address, timeoutMs);
                 sock.close();
                 return true;
-            } catch (IOException e){
+            } catch (IOException e) {
                 return false;
             }
         }
