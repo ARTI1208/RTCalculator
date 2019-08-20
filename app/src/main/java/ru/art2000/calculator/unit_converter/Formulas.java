e16456f0deea0ab576e35b7886b55192c807582d
ARTI1208 <31630000+ARTI1208@users.noreply.github.com>
2019-08-20 11:19:54 +0300

    [FIX][UNIT CONVERTER] Fixed volume conversion

--- app/src/main/java/ru/art2000/calculator/unit_converter/Formulas.java
+++ app/src/main/java/ru/art2000/calculator/unit_converter/Formulas.java
@@ -1,34 +1,37 @@
 package ru.art2000.calculator.unit_converter;
 
-import android.util.Log;
+class Formulas {
+    private static final int VELOCITY = 0;
+    private static final int DISTANCE = 1;
+    private static final int AREA = 2;
+    private static final int VOLUME = 3;
+    private static final int MASS = 4;
+    private static final int PRESSURE = 5;
+    private static final int TEMPERATURE = 6;
+    private final double[] velocityMultipliers = {1, 60, 60d / 1000d, 3600d / 1000d, 3.280839, 2.2369, 0.003, 1.9438};
+    private final double[] distanceMultipliers = {1000, 100, 10, 1, 1d / 1000d, 39.37, 3.2808, 1.0936, 0.00062137, 0.0005399568, 1d / 946000000d, 1d / 0.71};
+    private final double[] areaMultipliers = {1000000, 10000, 100, 1};
+    private final double[] volumeMultipliers = {1000000, 1};
+    private final double[] massMultipliers = {1000, 1};
+    private final double[] pressureMultipliers = {101325, 1};
+    private double[] velocityResult = {1, 60, 60d / 1000d, 3600d / 1000d, 3.280839, 2.2369, 0.003, 1.9438};
+    private double[] distanceResult = {1000, 100, 10, 1, 1 / 1000d, 39.37, 3.2808, 1.0936, 0.00062137, 0.0005399568, 1 / 946000000d, 1 / 0.71};
+    private double[] areaResult = {1000000, 10000, 100, 1};
+    private double[] volumeResult = {1000000, 1};
+    private double[] massResult = {1000, 1};
+    private double[] pressureResult = {101325, 1};
+    private double[] temperatureResult = {1, 2};
+    private FormulaItem celsius = new FormulaItem("Cel");
+    private FormulaItem far = new FormulaItem("Far");
+    private FormulaItem[] temps = new FormulaItem[]{celsius, far};
 
-public class Formulas {
-    public static final int VELOCITY = 0;
-    public static final int DISTANCE = 1;
-    public static final int AREA = 2;
-    public static final int VOLUME = 3;
-    public static final int MASS = 4;
-    public static final int PRESSURE = 5;
-    public static final int TEMPERATURE = 6;
-    final double[] velocityMultipliers = {1, 60, 60d/1000d, 3600d/1000d, 3.280839, 2.2369, 0.003, 1.9438};
-    final double[] distanceMultipliers = {1000, 100, 10, 1, 1d/1000d, 39.37, 3.2808, 1.0936, 0.00062137, 0.0005399568, 1d/946000000d, 1d/0.71};
-    final double[] areaMultipliers = {1000000, 10000, 100, 1};
-    final double[] volumeMultipliers = {1000000, 1};
-    final double[] massMultipliers = {1000, 1};
-    final double[] pressureMultipliers = {101325, 1};
-    double[] velocityResult = {1, 60, 60d/1000d, 3600d/1000d, 3.280839, 2.2369, 0.003, 1.9438};
-    double[] distanceResult = {1000, 100, 10, 1, 1/1000, 39.37, 3.2808, 1.0936, 0.00062137, 0.0005399568, 1/946000000, 1/0.71};
-    double[] areaResult = {1000000, 10000, 100, 1};
-    double[] volumeResult = {1000000, 1};
-    double[] massResult = {1000, 1};
-    double[] pressureResult = {101325, 1};
-    double[] temperatureResult = {1, 2};
-    FormulaItem celsius = new FormulaItem("Cel");
-    FormulaItem far = new FormulaItem("Far");
-    FormulaItem[] temps = new FormulaItem[]{celsius, far};
+    Formulas() {
+        celsius.addFormula(far.getTag(), "5÷9×(X-32)");
+        far.addFormula(celsius.getTag(), "9÷5×X+32");
+    }
 
-    public static int getCategoryInt(String str){
-        switch (str){
+    static int getCategoryInt(String str) {
+        switch (str) {
             default:
             case "velocity":
                 return VELOCITY;
@@ -47,12 +50,7 @@ public class Formulas {
         }
     }
 
-    Formulas(){
-        celsius.addFormula(far.getTag(), "5÷9×(X-32)");
-        far.addFormula(celsius.getTag(), "9÷5×X+32");
-    }
-
-    double getResult(int category, int pos){
+    double getResult(int category, int pos) {
         switch (category) {
             default:
             case VELOCITY:
@@ -72,34 +70,8 @@ public class Formulas {
         }
     }
 
-    public double calculateResult(int category, int pos, double value, int it){
-        Log.d("val", String.valueOf(value));
-        calc(category, pos, value);
-        try {
-            switch (category) {
-                case VELOCITY:
-                    return velocityResult[it];
-                case DISTANCE:
-                    return distanceResult[it];
-                case AREA:
-                    return areaResult[it];
-                case VOLUME:
-                    return volumeResult[it];
-                case MASS:
-                    return massResult[it];
-                case PRESSURE:
-                    return pressureResult[it];
-                case TEMPERATURE:
-                    return temperatureResult[it];
-            }
-        } catch (Exception e){
-            return 0.0;
-        }
-        return 0.0;
-    }
-
-    public void calc(int category, int pos, double value){
-        switch (category){
+    void calc(int category, int pos, double value) {
+        switch (category) {
             case VELOCITY:
                 calc(velocityMultipliers, velocityResult, pos, value);
                 break;
@@ -110,7 +82,7 @@ public class Formulas {
                 calc(areaMultipliers, areaResult, pos, value);
                 break;
             case VOLUME:
-                calc(volumeMultipliers, velocityResult, pos, value);
+                calc(volumeMultipliers, volumeResult, pos, value);
                 break;
             case MASS:
                 calc(massMultipliers, massResult, pos, value);
@@ -119,14 +91,12 @@ public class Formulas {
                 calc(pressureMultipliers, pressureResult, pos, value);
                 break;
             case TEMPERATURE:
-                Log.d("val", String.valueOf(value));
                 calcByFormula(temps, temperatureResult, pos, value);
                 break;
         }
     }
 
-    private void calc(double[] mults, double[] res, int pos, double value){
-        Log.d("rrr", String.valueOf(pos) );
+    private void calc(double[] mults, double[] res, int pos, double value) {
         int size = mults.length;
         if (pos < size) {
             double m = value / mults[pos];
@@ -137,13 +107,13 @@ public class Formulas {
         }
     }
 
-    private void calcByFormula(FormulaItem[] massive, double[] res, int pos, double value){
+    private void calcByFormula(FormulaItem[] massive, double[] res, int pos, double value) {
         for (int i = 0, size = res.length; i < size; i++) {
-            if (pos != i){
+            if (pos != i) {
                 res[i] = massive[i].convert(massive[pos].getTag(), value);
-            } else
+            } else {
                 res[pos] = value;
-//            Log.d(massive[i].getTag(), String.valueOf(value));
+            }
         }
     }
 }
