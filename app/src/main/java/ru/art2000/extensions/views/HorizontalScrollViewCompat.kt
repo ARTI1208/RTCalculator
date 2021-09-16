2e18c668619fac295b0b92bb693cc2e2a1caaa49
Art Pinch <leonardo906@mail.ru>
2021-09-16 12:18:24 +0300

    Fix wrong text input scrolling on API <21

---
+++ app/src/main/java/ru/art2000/extensions/views/HorizontalScrollViewCompat.kt
@@ -0,0 +1,73 @@
+package ru.art2000.extensions.views
+
+import android.R.attr
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.Gravity
+import android.widget.HorizontalScrollView
+
+/**
+ * Adopted from https://stackoverflow.com/a/20033587
+ */
+class HorizontalScrollViewCompat : HorizontalScrollView {
+
+    private var mGravityRight = false
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    )
+
+    @SuppressLint("RtlHardcoded")
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            super.onLayout(changed, left, top, right, bottom)
+        } else {
+            val childView = getChildAt(0)
+            super.onLayout(changed, left, top, right, bottom)
+            val p = childView.layoutParams as LayoutParams
+            val horizontalGravity = p.gravity and Gravity.HORIZONTAL_GRAVITY_MASK
+            val verticalGravity = p.gravity and Gravity.VERTICAL_GRAVITY_MASK
+            if (isRightGravity(horizontalGravity)) {
+                if (getScrollRange() > 0) {
+                    mGravityRight = true
+                    p.gravity =
+                        (if (horizontalGravity == Gravity.RIGHT) Gravity.LEFT else Gravity.END) or verticalGravity
+                    childView.layoutParams = p
+                    val layoutLeft = 0
+                    val layoutRight = 0
+                    super.onLayout(changed, layoutLeft, top, layoutRight, bottom)
+                }
+            } else if (mGravityRight) {
+                if (getScrollRange() == 0) {
+                    mGravityRight = false
+                    p.gravity =
+                        (if (horizontalGravity == Gravity.LEFT) Gravity.RIGHT else Gravity.END) or verticalGravity
+                    childView.layoutParams = p
+                    super.onLayout(changed, attr.left, top, attr.right, bottom)
+                }
+            }
+        }
+    }
+
+    @SuppressLint("RtlHardcoded")
+    private fun isRightGravity(gravity: Int): Boolean {
+        return gravity == Gravity.RIGHT || (gravity == Gravity.END && (layoutDirection == LAYOUT_DIRECTION_LTR))
+    }
+
+    private fun getScrollRange(): Int {
+        var scrollRange = 0
+        if (childCount > 0) {
+            val child = getChildAt(0)
+            scrollRange = 0.coerceAtLeast(child.width - (width - paddingLeft - paddingRight))
+        }
+        return scrollRange
+    }
+}
