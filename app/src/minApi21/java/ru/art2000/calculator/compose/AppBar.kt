5aba0528140c3e99a0598b9d7bc2ab8f74d902ce
Art Pinch <leonardo906@mail.ru>
2021-12-25 18:17:36 +0300

    Cleanup

--- app/src/minApi21/java/ru/art2000/calculator/compose/AppBar.kt
+++ app/src/minApi21/java/ru/art2000/calculator/compose/AppBar.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
