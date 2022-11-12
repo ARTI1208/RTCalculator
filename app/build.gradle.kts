b9d88e4d216d3cc3da5a1462d6defb3a77b74ecd
Art Pinch <leonardo906@mail.ru>
2022-11-12 15:53:28 +0300

    Update compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ plugins {
 
 setupAndroidModule()
 
-val composeVersion = "1.3.0"
-val composeMaterial3Version = "1.0.0"
+val composeVersion = "1.3.1"
+val composeMaterial3Version = "1.0.1"
 val composeCompilerVersion = "1.3.2"
 
 val major = 1
