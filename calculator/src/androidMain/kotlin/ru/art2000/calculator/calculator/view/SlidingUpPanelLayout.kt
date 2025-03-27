769275d95fd4515260d5036e9176e64f4b0b653f
Art Pinch <leonardo906@mail.ru>
2025-03-27 22:57:14 +0300

    Update dependencies, compile and target SDK versions

--- calculator/src/androidMain/kotlin/ru/art2000/calculator/calculator/view/SlidingUpPanelLayout.kt
+++ calculator/src/androidMain/kotlin/ru/art2000/calculator/calculator/view/SlidingUpPanelLayout.kt
@@ -3,6 +3,6 @@ package ru.art2000.calculator.calculator.view
 import com.sothree.slidinguppanel.PanelState
 import com.sothree.slidinguppanel.SlidingUpPanelLayout
 
-var SlidingUpPanelLayout.panelState: PanelState
-    get() = getPanelState()
+var SlidingUpPanelLayout.state: PanelState
+    get() = panelState
     set(value) = setPanelState(value)
