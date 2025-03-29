63316db1813db6a9a2e5113a119808b3164e9c21
Art Pinch <leonardo906@mail.ru>
2025-03-29 18:28:06 +0300

    Use viewbindingdelegate v2: Fix compile

--- app/src/main/java/ru/art2000/calculator/main/UpdateActivity.kt
+++ app/src/main/java/ru/art2000/calculator/main/UpdateActivity.kt
@@ -3,19 +3,17 @@ package ru.art2000.calculator.main
 import android.content.Intent
 import android.os.Bundle
 import androidx.core.net.toUri
-import by.kirich1409.viewbindingdelegate.CreateMethod
-import by.kirich1409.viewbindingdelegate.viewBinding
+import dev.androidbroadcast.vbpd.viewBinding
+import ru.art2000.calculator.R
 import ru.art2000.calculator.common.view.AppActivity
 import ru.art2000.calculator.databinding.ActivityUpdateBinding
 
-class UpdateActivity : AppActivity() {
+class UpdateActivity : AppActivity(R.layout.activity_update) {
 
-    private val binding by viewBinding<ActivityUpdateBinding>(CreateMethod.INFLATE)
+    private val binding by viewBinding(ActivityUpdateBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(binding.root)
 
         binding.downloadUpdateButton.setOnClickListener {
             startActivity(Intent(
